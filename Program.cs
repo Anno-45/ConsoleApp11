58f89b5ca8eb86e653eab065917eff48ac2e3893
Anno-45 <nikita20061108@mail.ru>
2025-02-14 13:51:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите три числа:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            double minimum = GetMinimum(num1, num2, num3);
+            Console.WriteLine($"Минимальное число: {minimum}");
+
+        }
+        static double GetMinimum(double a, double b, double c)
+        {
+            double min = a; // Предполагаем, что a - минимальное
+            if (b < min)
+            {
+                min = b; // Если b меньше, обновляем min
+            }
+            if (c < min)
+            {
+                min = c; // Если c меньше, обновляем min
+            }
+            return min; // Возвращаем минимальное число
+
+
+
+
+
+        }
+    }
+}
